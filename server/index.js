171c4c38d6dcb3bbd87c8e8f7ee2a31242af644b
User <user@ASLM069-TYE-RD.local>
2024-09-10 17:01:39 +0800

    First commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoute = require('./Routes/userRoute');
+
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/users', userRoute);
+
+// CRUD
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our chat app APIs');
+});
+
+const port = process.env.PORT || 5001;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port: ${port}`);
+});
+
+mongoose.connect(uri).then(() => console.log('MongoDB connection established'))
+    .catch((error) => console.log('MongoDB connection failed: ' + error.message));
+
