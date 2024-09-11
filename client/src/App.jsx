ef2c974d8c21cf8c5e14ac907e4efdaa7dbb28e7
yeen <yeenxdd@gmail.com>
2024-09-11 09:53:05 +0800

    UI for login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,24 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './pages/Chat';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <>
-    <Routes>
-      <Route path='/' element={<Chat />}></Route>
-      <Route path='/register' element={<Register />}></Route>
-      <Route path='/login' element={<Login />}></Route>
-      <Route path='*' element={<Navigate to='/' />}></Route>
-    </Routes>
-  </>
+  return (
+    <>
+    <NavBar />
+      <Container>
+        <Routes>
+          <Route path='/' element={<Chat />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='*' element={<Navigate to='/' />}></Route>
+        </Routes>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,29 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Login = () => {
-    return ( <>Login</> );
+    return (
+        <>
+            <Form>
+                <Row style={{
+                    height: "100vh",
+                    justifyContent: "Center",
+                    paddingTop: "10%",
+                }}>
+                    <Col xs={6}>
+                        <Stack gap={3}>
+                            <h2>Login</h2>
+
+                            <Form.Control type="email" placeholder="Email" />
+                            <Form.Control type="password" placeholder="Password" />
+                            <Button variant="primary" type="submit">Login</Button>
+
+                            <Alert variant="danger"><p>An error occured</p></Alert>
+                        </Stack>
+                    </Col>
+                </Row>
+            </Form>
+        </>
+    );
 }
- 
+
 export default Login;
